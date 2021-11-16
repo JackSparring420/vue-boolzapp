9ced9773892680481a9ce7351f86fac94af11c4f
JackSparring420 <emanuele.arconte94@gmail.com>
2021-11-16 01:22:46 +0100

    completed milestone 2

--- js/myscript.js
+++ js/myscript.js
@@ -2,6 +2,7 @@ var app = new Vue (
     {
         el: "#container",
         data:{
+            activeChat: 0,
             contacts: [
                 {
                     name: "Michele",
@@ -11,83 +12,108 @@ var app = new Vue (
                         {
                             date: ' 10/01/2020 1 5:30:55',
                             text: ' Hai portato a spasso i l cane?',
-                            status: ' sent'
+                            status: 'sent'
                         },
                         {
                             date: ' 10/01/2020 1 5:50:00',
                             text: ' Ricordati di dargli da mangiare',
-                            status: ' sent'
+                            status: 'sent'
                         },
                         {
                             date: ' 10/01/2020 1 6:15:22',
                             text: ' Tutto fatto!',
-                            status: ' received'
+                            status: 'received'
                         }
                     ],
                 },
                 {
                     name: "Fabio",
                     avatar: "img/avatar_2.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: ' 20/03/2020 1 6:30:00',
                             text: ' Ciao come stai?',
-                            status: ' sent'
+                            status: 'sent'
                         },
                         {
                             date: ' 20/03/2020 1 6:30:55',
                             text: ' Bene grazie! Stasera ci vediamo?',
-                            status: ' received'
+                            status: 'received'
                         },
                         {
                             date: ' 20/03/2020 1 6:35:00',
                             text: ' Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: ' sent'
+                            status: 'sent'
                         }
                     ],
                 },
                 {
                     name: "Samuele",
                     avatar: "img/avatar_3.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: ' 28/03/2020 1 0:10:40',
                             text: ' La Marianna va in campagna',
-                            status: ' received'
+                            status: 'received'
                         },
                         {
                             date: ' 28/03/2020 1 0:20:10',
                             text: ' Sicuro di non a ver sbagliato chat?',
-                            status: ' sent'
+                            status: 'sent'
                         },
                         {
                             date: ' 28/03/2020 1 6:15:22',
                             text: ' Ah scusa!',
-                            status: ' received'
+                            status: 'received'
                         }
                     ],
                 },
                 {
                     name: "Luisa",
                     avatar: "img/avatar_4.jpg",
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: ' 10/01/2020 1 5:30:55',
                             text: ' Lo sai che ha aperto una n uova pizzeria?',
-                            status: ' sent'
+                            status: 'sent'
                         },
                         {
                             date: ' 10/01/2020 1 5:50:00',
                             text: ' Si, ma preferirei andare a l cinema',
-                            status: ' received'
+                            status: 'received'
                         },
                     ],
                 },
 
             ],
         },
+       
+        methods: {
+            changeChat(index){
+                if(this.contacts[0].visible === true){
+                    this.contacts[0].visible = false
+                };
+                if(this.contacts[1].visible === true){
+                    this.contacts[1].visible = false
+                };
+                if(this.contacts[2].visible === true){
+                    this.contacts[2].visible = false
+                };
+                if(this.contacts[3].visible === true){
+                    this.contacts[3].visible = false
+                };
+
+                if (this.contacts[index].visible !== true){
+                    this.contacts[index].visible = true
+                } else {
+                    this.contacts[index].visible = false
+                };
+
+                this.activeChat = index
+            }
+        }
     }
 );
